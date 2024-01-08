0da1bc562d6f83a19686be57db1726c078e8655a
GouravKumar13 <gouravkumarat@gmail.com>
2024-01-09 00:08:32 +0530

    fix import

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,18 @@
 // import React from 'react'
 import Nishyanlogo from "../assets/nishyan.png"
 import arrowDown from "../assets/Icons/arrowdown.png"
-import { Home, Analytics, Order, Products, Delivery, Marketing, payments, tools, discount, audience, appearance, plugins } from "../assets/index"
+import Home from '../assets/icons/Home.png'
+import Analytics from "../assets/Icons/Analytics.png"
+import Order from "../assets/Icons/orders.png"
+import Products from "../assets/Icons/products.png"
+import Delivery from "../assets/Icons//Dilivary.png"
+import Marketing from "../assets/Icons/marketing.png"
+import payments from "../assets/Icons/payments.png"
+import tools from "../assets/Icons/tools.png"
+import discount from "../assets/Icons/discount.png"
+import audience from "../assets/Icons/Audiance.png"
+import appearance from "../assets/Icons/Appearance.png"
+import plugins from "../assets/Icons/plugins.png"
 import wallet from "../assets/Icons/wallet.png"
 const SideBar = () => {
     const utils = [
